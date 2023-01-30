5bd6d2d85943cfbaaa8a6898458500d1fa79a256
ajit0507 <ajitshivajipatil@gmail.com>
2023-01-30 12:33:38 +0530

    first commit

---
+++ src/main/java/com/example/demo/CicdWithDockerHubSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWithDockerHubSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWithDockerHubSpringBootApplication.class, args);
+	}
+
+}
